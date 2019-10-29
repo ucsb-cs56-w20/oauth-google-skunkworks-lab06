47f81a35e6f0ec7ef5abb1de199da66214ad84dc
Phill Conrad <pconrad@cs.ucsb.edu>
2019-10-29 14:44:18 -0700

    updated for github login but no logout

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,23 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/oauth_login").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .oauth2Login()
+        .loginPage("/oauth_login");
     }
 
 }
---
+++ src/main/java/hello/WebController.java
@@ -0,0 +1,49 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
+
+@Controller
+public class WebController {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @GetMapping("/oauth_login")
+    public String getLoginPage(Model model) {
+        Map<String, String> urls = new HashMap<>();
+
+        Iterable<ClientRegistration> iterable;
+        iterable = ((Iterable<ClientRegistration>) clientRegistrationRepository);
+        iterable.forEach(clientRegistration -> urls.put(clientRegistration.getClientName(),
+                "/oauth2/authorization/" + clientRegistration.getRegistrationId()));
+
+        model.addAttribute("urls", urls);
+        return "oauth_login";
+    }
+
+    @GetMapping("/info")
+    public String getInfo(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
+
+        // https://developer.github.com/v3/users/#get-the-authenticated-user
+
+        model.addAttribute("name", oAuth2User.getAttributes().get("name"));
+        model.addAttribute("login", oAuth2User.getAttributes().get("login"));
+        model.addAttribute("id", oAuth2User.getAttributes().get("id"));
+        model.addAttribute("avatar_url", oAuth2User.getAttributes().get("avatar_url"));
+
+        return "info";
+    }
+
+}
